d8b44e8edbc3d70e4197d36627b71cf143c51556
MuhammadUmer1270 <muhammadumer56251@gmail.com>
2024-12-23 22:48:53 +0500

    mian_file config with firebase

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp/colors.dart';
+import 'package:whatsapp/features/common/widget/utilis/loader.dart';
+// import 'package:whatsapp/features/common/widget/utilis/loader.dart';
+// import 'package:whatsapp/features/common/widget/utilis/loader.dart';
+import 'package:whatsapp/features/landing_screen.dart';
+import 'package:whatsapp/repository/controler.dart';
+import 'package:whatsapp/screens/Forget.dart';
+import 'package:whatsapp/screens/Login.dart';
+// import 'package:whatsapp/repository/controler.dart';
+import 'package:whatsapp/screens/OTP_Screen.dart';
+import 'package:whatsapp/screens/Signup.dart';
+import 'package:whatsapp/screens/errorscreen.dart';
+// import 'package:whatsapp/screens/errorscreen.dart';
+import 'package:whatsapp/screens/login_Screen.dart';
+import 'package:whatsapp/features/chat/mobliechat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:whatsapp/screens/moblielayout.dart';
+import 'package:whatsapp/screens/select_contact_screen.dart';
+import 'package:whatsapp/screens/user_info_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: backgroundColor, primaryColor: appBarColor),
+      title: 'Whatsapp',
+      home: ref.watch(userdatauthprovider).when(
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              } else {
+                return const moblielayout();
+              }
+            },
+            error: (error, stackTrace) {
+              return ErrorScreen(
+                errorMessage: "can't loading ",
+                onRetry: () {
+                  Navigator.pushNamed(context, LandingScreen.id);
+                },
+              );
+            },
+            loading: () => const loader(),
+          ),
+      // routes: {
+      //   Selectcontactlist.id: (context) => Selectcontactlist(),
+      //   LandingScreen.id: (context) => const LandingScreen(),
+      //   MoblieChatScreen.id: (context) => const MoblieChatScreen(),
+      //   Loginscreen.id: (context) => const Loginscreen(),
+      //   OTPScreen.id: (context) => OTPScreen(),
+      //   Userinfo.id: (context) => const Userinfo(),
+      //   ErrorScreen.id: (context) => ErrorScreen(
+      //       errorMessage: "errorMessage",
+      //       onRetry: () {
+      //         Navigator.pushNamed(context, LandingScreen.id);
+      //       }),
+      //       Login.id:(context)=>Login(),
+      //       Signup.id:(context)=>Signup(),
+      //       Forget.id:(context)=>Forget(),
+      // },
+    );
+  }
+}
